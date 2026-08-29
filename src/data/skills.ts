01bcbb925506437bca1b5c502dcfb0567fdf35f3
Pedro Henrique <pedrohenriquezzz2022@gmail.com>
2026-08-29 18:12:39 -0300

    atualização de Competências

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -2,7 +2,7 @@ import type { Skill } from "../types";
 
 // 2. Dados (Simulando o banco)
 export const skills: Skill[] = [
-    { title: "Excel Avançado", description: "Domínio completo em fórmulas complexas.", items: ["Fórmulas Avançadas", "Tabelas Dinâmicas", "VBA e Macros"] },
+    { title: "Excel Avançado", description: "Domínio completo em fórmulas complexas.", items: ["Fórmulas Avançadas", "Tabelas Dinâmicas"] },
     { title: "Power BI", description: "Criação de dashboards interativos.", items: ["DAX", "Power Query", "Modelagem"] },
     { title: "PostgreSQL", description: "Design e gerenciamento de bancos relacionais.", items: ["Consultas Otimizadas", "Administração", "Modelagem"] },
     { title: "Google Sheets", description: "Planilhas colaborativas e automação de dados.", items: ["Fórmulas", "Tabelas Dinâmicas", "Apps Script"] },
